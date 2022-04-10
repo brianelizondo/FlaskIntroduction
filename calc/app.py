ca260583669d56cff78293b5e46bd534f8f2c16e
Brian Elizondo <brian_elizondo@msn.com>
2022-04-09 23:19:02 -0400

    Further Study => Make a single route/view function that can deal with 4 different kinds of URLs, write this in one function with one route by using a route parameter for the actual operation. As an extra-bonus, see if you can find a way to do this in the route without a whole series of if/elif statements

--- calc/app.py
+++ calc/app.py
@@ -27,3 +27,20 @@ def dividing():
     a = int(request.args["a"])
     b = int(request.args["b"])
     return f"<h1>a / b = {div(a, b)}</h1>"
+
+
+# FURTHER STUDY 
+# =========================================
+OPERATIONS = {
+    "add": add,
+    "sub": sub,
+    "mult": mult,
+    "div": div,
+}
+
+@app.route('/math/<view>')
+def math_operations(view):
+    a = int(request.args["a"])
+    b = int(request.args["b"])
+    result = OPERATIONS[view](a, b)
+    return f"The result of '{view}' operation is = {result}</h1>"
