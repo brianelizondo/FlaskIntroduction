fa9e19db8f73821b2d9a1e95bb7f5df02f592b92
Brian Elizondo <brian_elizondo@msn.com>
2022-04-09 23:05:03 -0400

    Calc => Build a simple calculator with Flask, which uses URL query parameters to get the numbers to calculate with

--- calc/app.py
+++ calc/app.py
@@ -1,1 +1,29 @@
 # Put your app in here.
+from flask import Flask, request
+from operations import *
+
+app = Flask(__name__)
+
+@app.route('/add')
+def adds():
+    a = int(request.args["a"])
+    b = int(request.args["b"])
+    return f"<h1>a + b = {add(a, b)}</h1>"
+
+@app.route('/sub')
+def subtracting():
+    a = int(request.args["a"])
+    b = int(request.args["b"])
+    return f"<h1>a - b = {sub(a, b)}</h1>"
+
+@app.route('/mult')
+def multiplying():
+    a = int(request.args["a"])
+    b = int(request.args["b"])
+    return f"<h1>a * b = {mult(a, b)}</h1>"
+
+@app.route('/div')
+def dividing():
+    a = int(request.args["a"])
+    b = int(request.args["b"])
+    return f"<h1>a / b = {div(a, b)}</h1>"
